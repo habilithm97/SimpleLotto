06aad1f492674bd578ea4004f21594525a46d2ce
habilithm97 <habinkim97@gmail.com>
2023-11-09 13:31:53 +0900

    랜덤 로또 번호 생성

--- app/src/main/java/com/example/simplelotto/MainActivity.kt
+++ app/src/main/java/com/example/simplelotto/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.simplelotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.simplelotto.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.btnRun.setOnClickListener {
+            val lottoNums = createLottoNumber()
+            Log.d("TAG", lottoNums.toString())
+        }
+    }
+
+    private fun createLottoNumber() : List<Int> {
+        val result = mutableListOf<Int>()
+        val nums = IntArray(45) {it + 1}
+        nums.shuffle(Random(System.currentTimeMillis()))
+        nums.slice(0..5).forEach {num -> result.add(num)}
+        result.sort()
+
+        return result
     }
 }
