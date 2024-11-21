0cc1ecbdd2b0523f61fda95be40deeef90d5dcac
habilithm97 <habinkim97@gmail.com>
2024-11-21 18:04:26 +0900

    로또볼 색상

--- app/src/main/java/com/example/simplelotto/ui/fragment/Fragment1.kt
+++ app/src/main/java/com/example/simplelotto/ui/fragment/Fragment1.kt
@@ -1,5 +1,6 @@
 package com.example.simplelotto.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,17 +8,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.example.simplelotto.R
 import com.example.simplelotto.databinding.Fragment1Binding
+import com.example.simplelotto.ui.activity.MainActivity
 import kotlin.random.Random
 
 class Fragment1 : Fragment() {
     private var _binding: Fragment1Binding? = null
     private val binding get() = _binding!!
+    private lateinit var mContext: MainActivity
 
     private val ballList : List<TextView> by lazy {
         with(binding) {
             listOf(ball1, ball2, ball3, ball4, ball5, ball6)
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mContext = context as MainActivity
     }
 
     override fun onCreateView(
@@ -34,8 +45,12 @@ class Fragment1 : Fragment() {
         binding.apply {
             btn.setOnClickListener {
                 val lottoNumbers = createLottoNumbers()
-                //Log.d("Fragment1", lottoNumbers.toString())
                 setLottoNumbers(lottoNumbers)
+
+                lottoNumbers.forEachIndexed { index, number ->
+                    val ball = ballList[index]
+                    setBallColor(number, ball)
+                }
             }
         }
     }
@@ -55,6 +70,17 @@ class Fragment1 : Fragment() {
         ballList.forEachIndexed { index, textView ->
             textView.text = result[index].toString()
         }
+    }
+
+    private fun setBallColor(number: Int, ball: TextView) {
+        val ballColor = when (number) {
+            in 1..10 -> R.drawable.ball_yellow
+            in 11..20 -> R.drawable.ball_blue
+            in 21..30 -> R.drawable.ball_red
+            in 31..40 -> R.drawable.ball_gray
+            else -> R.drawable.ball_green
+        }
+        ball.background = ContextCompat.getDrawable(mContext, ballColor)
     }
 
     override fun onDestroyView() {
