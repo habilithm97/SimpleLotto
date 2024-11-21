d960abf68fa6cb34a8e455023a728caf9a4b8434
habilithm97 <habinkim97@gmail.com>
2024-11-21 13:04:01 +0900

    로또 추첨

--- app/src/main/java/com/example/simplelotto/ui/fragment/Fragment1.kt
+++ app/src/main/java/com/example/simplelotto/ui/fragment/Fragment1.kt
@@ -1,11 +1,13 @@
 package com.example.simplelotto.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.simplelotto.databinding.Fragment1Binding
+import kotlin.random.Random
 
 class Fragment1 : Fragment() {
     private var _binding: Fragment1Binding? = null
@@ -23,8 +25,21 @@ class Fragment1 : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-
+            btn.setOnClickListener {
+                val lottoNumbers = createLottoNumbers()
+                Log.d("Fragment1", lottoNumbers.toString())
+            }
         }
+    }
+
+    private fun createLottoNumbers() : List<Int> {
+        val result = mutableListOf<Int>()
+        val numbers = IntArray(45) { it + 1 }
+        numbers.shuffle(Random(System.currentTimeMillis()))
+        numbers.slice(0..5).forEach { number -> result.add(number) }
+        result.sort()
+
+        return result
     }
 
     override fun onDestroyView() {
