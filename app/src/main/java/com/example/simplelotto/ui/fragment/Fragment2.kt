268685ead275d5352cc876a40cce6b9c05d7f3b1
habilithm97 <habinkim97@gmail.com>
2024-11-24 23:51:22 +0900

    로또 추첨기 / 당첨 번호 조회 화면 수정

--- app/src/main/java/com/example/simplelotto/ui/fragment/Fragment2.kt
+++ app/src/main/java/com/example/simplelotto/ui/fragment/Fragment2.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.simplelotto.databinding.Fragment2Binding
 
 class Fragment2 : Fragment() {
